00965b489ea424457170ca9819381a803df8f953
Raif Harik <reharik@gmail.coml7k>
2012-10-16 06:31:33 -0500

    adding tests to js

--- src/MethodFitness.Web/Content/scripts/MF/MF.mixins.js
+++ src/MethodFitness.Web/Content/scripts/MF/MF.mixins.js
@@ -5,6 +5,14 @@
  * Time: 9:53 AM
  * To change this template use File | Settings | File Templates.
  */
+if (typeof MF == "undefined") {
+    var MF = {};
+}
+
+if (typeof CC == "undefined") {
+    var CC = {};
+}
+
 MF.mixins = {};
 
 MF.mixin = function(target, mixin, preserveRender){
@@ -40,13 +48,14 @@ MF.mixins.modelAndElementsMixin = {
         }
 
         var ignore = _.filter(_.keys(this.model),function(item){
-            return (item.indexOf('_') == 0 && item != "__ko_mapping__");
+            return (item.indexOf('_') == 0 && item != "__ko_mapping__" );
         });
         _.each(ignore,function(item){
             that.mappingOptions.ignore.push(item);});
         this.renderElements();
         this.mappingOptions.ignore.push("_availableItems");
         this.mappingOptions.ignore.push("_resultsItems");
+        this.mappingOptions.ignore = _.uniq(this.mappingOptions.ignore);
         MF.vent.trigger("model:"+this.id+"modelLoaded");
     },
     bindSpecificModelAndElements:function(viewModel){
@@ -78,7 +87,7 @@ MF.mixins.modelAndElementsMixin = {
         }, this);
         model._title = ko.computed(function(){
             if(model.EntityId()<=0 && model._Title()){
-                return "Add New "+model._Title();
+                return "Add New "+model._Title_Title();
             }
             return model._Title() ? model._Title() : "";
         },this);
