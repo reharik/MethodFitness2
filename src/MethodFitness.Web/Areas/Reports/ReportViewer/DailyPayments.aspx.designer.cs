0c84558526581e430d85965c6e507cd0e99106b9
Raif Harik <reharik@gmail.com>
2013-01-27 15:54:50 -0600

    working on reports

--- src/MethodFitness.Web/Areas/Reports/ReportViewer/DailyPayments.aspx.designer.cs
+++ src/MethodFitness.Web/Areas/Reports/ReportViewer/DailyPayments.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace MethodFitness.Web.Areas.Reports.Reports {
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
         
         /// <summary>
+        /// SqlDataSource2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource2;
+        
+        /// <summary>
         /// ScriptManager1 control.
         /// </summary>
         /// <remarks>
