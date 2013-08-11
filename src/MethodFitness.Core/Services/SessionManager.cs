4011c6ce173b80025bbed99bac7e9a3214dc95ea
Raif Harik <reharik@gmail.com>
2013-08-11 09:49:32 -0500

    some changes I guess

---
+++ src/MethodFitness.Core/Services/SessionManager.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using CC.Core;
+using CC.Core.DomainTools;
+using MethodFitness.Core.Domain;
+using NHibernate.Linq;
+
+namespace MethodFitness.Core.Services
+{
+    public class SessionManager
+    {
+        private readonly IRepository _repository;
+        private IQueryable<Appointment> _appointments;
+
+        public SessionManager(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void GatherAppointmentsDue()
+        {
+            // I don't know why this is a field.
+            _appointments = _repository.Query<Appointment>(x=>x.EndTime < DateTime.Now && x.Completed)
+                .FetchMany(x=>x.Clients).ThenFetch(x=>x.Sessions);
+            _appointments.ForEachItem(x =>
+                {
+//                    do somthing
+                });
+        }
+
+        public void CompleteAppointments()
+        {
+            var appointments = _repository.Query<Appointment>(x => x.EndTime < DateTime.Now && !x.Completed)
+                .FetchMany(x => x.Clients).ThenFetch(x => x.Sessions);
+            appointments.ForEachItem(x =>
+                {
+                    x.SetSessionsForClients();
+
+                });
+        } 
+    }
+}
