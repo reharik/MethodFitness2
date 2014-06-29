a0e63f17a9953b01eb8f81e0a77233cffd6dab48
Raif Harik <reharik@gmail.com>
2014-06-29 08:26:26 -0500

    removing old controler that modifiles appointments by date

--- src/Projects/MF.Web/Controllers/ScheduledTasksController.cs
+++ src/Projects/MF.Web/Controllers/ScheduledTasksController.cs
@@ -1,36 +1,36 @@
-﻿using System;
-using System.Web.Mvc;
-using CC.Core;
-using CC.Core.DomainTools;
-using CC.Utility;
-using MF.Core.Domain;
-using StructureMap;
-
-namespace MF.Web.Controllers
-{
-    public class ScheduledTasksController : Controller
-    {
-        private IRepository _repository;
-
-        public ScheduledTasksController()
-        {
-            _repository = ObjectFactory.Container.GetInstance<IRepository>("NoInterceptorNoFiltersUnitOfWork");
-        }
-
-        public ActionResult ProcessAppointments()
-        {
-            var appointments = _repository.Query<Appointment>(x => x.EndTime > DateTime.Now.AddDays(-1) && x.EndTime <= DateTime.Now);
-            appointments.ForEachItem(x =>
-                                  {
-                                      x.Sessions.ForEachItem(s =>
-                                                          {
-                                                              s.SessionUsed = true;
-                                                              s.Trainer = x.Trainer;
-                                                              x.Trainer.AddAppointment(x);
-                                                          });
-                                      _repository.Save(x);
-                                  });
-            return null;
-        }
-    }
-}
+﻿//using System;
+//using System.Web.Mvc;
+//using CC.Core;
+//using CC.Core.DomainTools;
+//using CC.Utility;
+//using MF.Core.Domain;
+//using StructureMap;
+//
+//namespace MF.Web.Controllers
+//{
+//    public class ScheduledTasksController : Controller
+//    {
+//        private IRepository _repository;
+//
+//        public ScheduledTasksController()
+//        {
+//            _repository = ObjectFactory.Container.GetInstance<IRepository>("NoInterceptorNoFiltersUnitOfWork");
+//        }
+//
+//        public ActionResult ProcessAppointments()
+//        {
+//            var appointments = _repository.Query<Appointment>(x => x.EndTime > DateTime.Now.AddDays(-1) && x.EndTime <= DateTime.Now);
+//            appointments.ForEachItem(x =>
+//                                  {
+//                                      x.Sessions.ForEachItem(s =>
+//                                                          {
+//                                                              s.SessionUsed = true;
+//                                                              s.Trainer = x.Trainer;
+//                                                              x.Trainer.AddAppointment(x);
+//                                                          });
+//                                      _repository.Save(x);
+//                                  });
+//            return null;
+//        }
+//    }
+//}
