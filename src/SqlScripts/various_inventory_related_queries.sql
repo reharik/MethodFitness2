765d2063de6d2f10919d4518cdbd2f0529714be2
Raif Harik <reharik@gmail.com>
2016-02-05 18:11:39 -0600

    added more scripts and more data to existing script

---
+++ src/SqlScripts/various_inventory_related_queries.sql
@@ -0,0 +1,21 @@
+
+
+
+
+select * from appointment a where a.Completed =1 and not exists (select 1 from session s where a.EntityId = s.AppointmentId) order by a.date desc
+
+select * from appointment a inner join Appointment_Client ac on a.entityid = ac.AppointmentId where a.Completed =1 and ac.clientid = 2400 and not exists (select 1 from session s where a.EntityId = s.AppointmentId) order by a.date desc
+
+select * from appointment a inner join Appointment_Client ac on a.entityid = ac.AppointmentId where a.Completed =1 and not exists (select 1 from session s where a.EntityId = s.AppointmentId) order by a.date desc
+
+select * from appointment a inner join TrainerPaymentSessionItem tpsi on a.entityid = tpsi.AppointmentId where a.Completed =1 and not exists (select 1 from session s where a.EntityId = s.AppointmentId) order by clientid
+
+select * from TrainerPaymentSessionItem where appointmentId =25325
+
+select * from session s  where clientid = 2400 and appointmentid = 25325
+
+select * from Appointment_Client where appointmentid = 25421
+
+select * from session where clientid = 2400
+
+select * from client where entityid =2400
