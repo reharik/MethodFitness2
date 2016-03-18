d88920967bc32a16ddc3a742ab573fdc14b21e43
Raif Harik <reharik@gmail.com>
2016-03-18 12:16:41 -0500

    fixing a logging thing

---
+++ src/Projects/MF.SessionManagement/ServiceInstall.bat
@@ -0,0 +1,4 @@
+﻿
+MF.SessionManagement\NServiceBus.Host.exe /install /serviceName:"MF.SessionManagement.QA" /displayName:"MF.SessionManagement.QA" /description:"MF.SessionManagement.QA" 
+
+NET START "MF.SessionManagement.QA"
