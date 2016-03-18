a8b86ec99d29f346f64d630faba8f3822187061b
Raif Harik <reharik@gmail.com>
2016-03-18 10:23:17 -0500

    sql changes

---
+++ src/SqlScripts/inarrears_adj.sql
@@ -0,0 +1,21 @@
+
+
+-- select * from session where InArrears = 1 order by changeddate desc
+
+
+update session set SessionUsed = 1, 
+AppointmentId = 25455, 
+trainerid = 14 
+where entityid = 25517
+
+delete session 
+where entityid = 25588
+
+select * from session 
+where clientid = 2468
+order by changeddate desc
+
+
+
+
+
