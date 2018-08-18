1f7f5f787383c3ea68e87b678e5b17371ed459c6
Raif Harik <reharik@gmail.com>
2018-08-18 14:57:01 -0500

    horrible debugging

--- src/Projects/MF.Web/Menus/MainMenu.cs
+++ src/Projects/MF.Web/Menus/MainMenu.cs
@@ -6,23 +6,18 @@ using MF.Web.Areas.Billing.Controllers;
 using MF.Web.Areas.Reporting.Controllers;
 using MF.Web.Areas.Schedule.Controllers;
 using MF.Web.Controllers;
-using CC.Core.Security.Interfaces;
-using CC.Core.Security.Model;
-using MF.Core;
+
 namespace MF.Web.Menus
 {
     public class MainMenu : IMenuConfig
     {
         private readonly IMenuBuilder _builder;
         private readonly ISessionContext _sessionContext;
-		private readonly IAuthorizationRepository authorizationRepository;
-        private readonly ILogger _logger;
 
-        public MainMenu(IMenuBuilder builder, ISessionContext sessionContext, IAuthorizationRepository authorizationRepository, ILogger logger)
+        public MainMenu(IMenuBuilder builder, ISessionContext sessionContext)
         {
             _builder = builder;
             _sessionContext = sessionContext;
-            _logger = logger;
         }
 
         public IList<MenuItem> Build(bool withoutPermissions = false)
@@ -36,11 +31,6 @@ namespace MF.Web.Menus
             if (!withoutPermissions)
             {
                 user = _sessionContext.GetCurrentUser();
-                UsersGroup[] usersGroups = authorizationRepository.GetAssociatedUsersGroupFor(user);
-                foreach (var x in usersGroups)
-                {
-                    _logger.LogInfo(x.Name);
-                }
             }
             var builder =
                 _builder.CreateTagNode<AppointmentCalendarController>(WebLocalizationKeys.CALENDAR).Route("calendar")
