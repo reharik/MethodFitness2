27376476681e418ab6b0cc6d3ba7e6cfd01c6577
Raif Harik <reharik@gmail.com>
2020-05-24 12:20:04 -0500

    wip

--- src/Projects/MF.ClientAbsenteeReport/ClientAbsenteeJob.cs
+++ src/Projects/MF.ClientAbsenteeReport/ClientAbsenteeJob.cs
@@ -10,20 +10,27 @@ namespace MF.ClientAbsenteeReport
     {
         public void Execute(IJobExecutionContext context)
         {
-            var service = ObjectFactory.Container.GetInstance<IGetDroppedClients>();
             var logger = ObjectFactory.Container.GetInstance<ILogger>();
-            logger.LogError("about to check for clients");
-            var droppedClients = service.GetClients();
-            if (!droppedClients.Any())
+            try
             {
-                logger.LogInfo("No new dropped clients on: " + DateTime.Now.ToString());
-                return;
+                var service = ObjectFactory.Container.GetInstance<IGetDroppedClients>();
+                logger.LogError("about to check for clients");
+                var droppedClients = service.GetClients();
+                if (!droppedClients.Any())
+                {
+                    logger.LogInfo("No new dropped clients on: " + DateTime.Now.ToString());
+                    return;
+                }
+                var email = service.CreateEmail(droppedClients);
+                service.SendEmail(email, "absentee report");
+                service.UpdateClients(droppedClients);
+                logger.LogInfo("Job Processed at: " + DateTime.Now.ToString());
+                logger.LogDebug("{0}", email);
             }
-            var email = service.CreateEmail(droppedClients);
-            service.SendEmail(email, "absentee report");
-            service.UpdateClients(droppedClients);
-            logger.LogInfo("Job Processed at: "+ DateTime.Now.ToString());
-            logger.LogDebug("{0}", email);
+            catch (Exception ex)
+            {
+                logger.LogError("error", ex);
+            }
         }
     }
 }
