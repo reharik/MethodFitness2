43e298939e01e93611647cf65d15118b6189e132
Raif Harik <harik.raif@gmail.com>
2020-11-01 11:19:39 -0600

    killing dead projects

--- ServiceScripts/WeeklyManagerReport_PROD.bat
+++
@@ -1,4 +0,0 @@
-
-MF.WeeklyManagerReport_PROD\MF.WeeklyManagerReport.exe install -servicename:MF.WeeklyManagerReport.PROD -displayname:MF.WeeklyManagerReport.PROD
-
-NET START "MF.WeeklyManagerReport.PROD"
