627aec962b59cbd750ec866ef7792fe1a7932372
Raif Harik <harik.raif@gmail.com>
2022-10-02 09:37:10 -0500

    working on interop

---
+++ src/MF.Web/Content/scripts/internalHelpers/mf.repository2.js
@@ -0,0 +1,81 @@
+/**
+ * Created by JetBrains RubyMine.
+ * User: RHarik
+ * Date: 10/13/11
+ * Time: 10:52 AM
+ * To change this template use File | Settings | File Templates.
+ */
+ if (typeof MF == "undefined") {
+	var MF = {};
+}
+
+const validUrl = (url) => {
+	return (url || "").startsWith("/")
+		? `http://localhost:3002/csharp${url}`
+		: url;
+}
+
+MF.repository= (function(){
+	var repositoryCallback = function(result,callback){
+			if(result.LoggedOut){
+					window.location.replace(result.RedirectUrl);
+					return null;
+			}
+			clearTimeout(MF.throbberTimeout);
+			MF.showThrob=false;
+			$("#ajaxLoading").hide();
+			return result;
+	};
+	var throbber = function(){
+			MF.showThrob=true;
+			if(!MF.throbberTimeout){
+					MF.throbberTimeout = setTimeout(function() {
+							if(MF.showThrob) {
+									$("#ajaxLoading").show();
+							}
+					}, 500);
+			}
+	};
+	return {
+		ajaxPost: async function(url, data){
+			throbber();
+			const res = await fetch(validUrl(url), {
+				method: "POST",
+				body: data,
+				credentials: 'include',
+			});
+			const json = await res.json()
+			return repositoryCallback(json);
+	},
+		ajaxGet:async function(url, data){
+				throbber();
+			
+			const res = await fetch(validUrl(url) +"?"+ new URLSearchParams(data), {method:"GET", 
+			// headers: {
+			// 		"Content-Type": "text/html; charset=utf-8"
+			// 	},
+				credentials: 'include',
+			})
+      let body;
+			if(res.headers.get("content-type").includes("json")){
+        body = await res.json();
+			} else {
+        body = await res.text();
+			}
+			return repositoryCallback(body);
+		},
+		ajaxPostModel: async function(url, data){
+				throbber();
+			const res = await fetch(validUrl(url), {
+						method:"POST",
+						body:data,
+						headers: {
+							'Content-Type': "application/json; charset=utf-8",
+						},
+						credentials: 'include',
+			})
+			const json = await  res.json()
+			return repositoryCallback(json);
+		}
+	}
+}());
